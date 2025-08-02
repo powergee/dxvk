312e62f2aacd3092fcd8e3562024142df4b9f7e2
Jeonghyeon Kim <powergee101@gmail.com>
2025-08-03 02:48:04 +0900

    [test] Log RC modifications

--- src/util/rc/util_rc.h
+++ src/util/rc/util_rc.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <atomic>
+#include <iostream>
 
 namespace dxvk {
   
@@ -16,6 +17,7 @@ namespace dxvk {
      * \returns New reference count
      */
     uint32_t incRef() {
+      std::cout << "RC increments (" << this << "\n";
       return ++m_refCount;
     }
     
@@ -24,6 +26,7 @@ namespace dxvk {
      * \returns New reference count
      */
     uint32_t decRef() {
+      std::cout << "RC decrements (" << this << "\n";
       return --m_refCount;
     }
     
